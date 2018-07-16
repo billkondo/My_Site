c4897d375f7a6df73cacd0bc30c817285e4dde29
billkondo <willkondo@gmail.com>
2018-07-16 01:43:57 -0300

    Added Pomodoro Clock Better Image Experience

--- src/components/PomodoroClock/PomodoroClock.js
+++ src/components/PomodoroClock/PomodoroClock.js
@@ -1,8 +1,35 @@
 import React from 'react';
+import Description from './../Description/Description';
 
-const PomodoroClock = () => (
-  <div id="pomodoro-clock">
-  </div>
-);
+class PomodoroClock extends React.Component {
+  state = {
+    description: false
+  }
+
+  flipState = () => 
+    this.setState((prevState) => {
+      return {
+        description: !prevState.description
+      };
+    });
+
+  render() {
+    return (
+      <div id="pomodoro-clock"
+        onMouseEnter={this.flipState}
+        onMouseLeave={this.flipState}
+      >
+        {
+          this.state.description && 
+          <Description 
+            title={"Pomodoro Clock"}
+            text={`Time to study ?`}
+            tags={['Javascript', 'CSS3', 'React', 'Redux', 'Audio']}
+          />
+        }
+      </div>
+    );
+  }
+}
 
 export default PomodoroClock;
