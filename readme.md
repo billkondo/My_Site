e69aa42d25b26d5fdfea9538e63b63d6f74bcdb7
billkondo <willkondo@gmail.com>
2018-07-24 22:52:03 -0300

    Added Readme, Fonts and changed Styles

---
+++ readme.md
@@ -0,0 +1,3 @@
+# My Site
+
+This is a site where I will put some projects. It's live on [Heroku](https://billkondo.herokuapp.com/).
--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -2,7 +2,38 @@ import React from 'react';
 
 const AboutMe = () => (
   <div id="about-me" className="contain-site">
-    
+    <div id="title-about-me">
+      <p> William Hideki Kondo </p>
+    </div>
+
+    <div id="description-about-me"> 
+      <p> 
+        <b>Hi</b>! I'm a computer science student on <a href="https://www.ime.usp.br/en" target="_blank">Institue of Mathematics and Statistics</a>, 
+        part of <a href="http://www5.usp.br/english/?lang=en" target="_blank">University of São Paulo</a>.
+      </p>
+
+      <p> 
+        I enjoy to build apps and learn new things in the process. I also like to take part on programming competitions. 
+      </p>
+    </div> 
+
+    <div id="site-about-me"> 
+      About this site
+    </div>
+
+    <div id="site-description-about-me">
+      <p> The purpose of this site is to show my projects and to train my programming skills. </p>
+      <p> The main technologies that I used to build this site are:</p>
+      <ul>
+        <li>React</li>
+        <li>React-Router</li>
+        <li>Redux</li>
+        <li>SCSS</li>
+      </ul>
+    </div>
+
+
+
   </div>
 );
 
